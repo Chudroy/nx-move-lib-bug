a69b64bf1d0580f22e7d34ecfe4612af5b3bea6d
ngaritagoitia <ngaritagoitia@irontec.com>
2024-12-16 13:40:51 +0100

    add libs

---
+++ libs/shared/test-lib-2/src/lib/test-lib-2/test-lib-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-test-lib-2',
+  imports: [CommonModule],
+  templateUrl: './test-lib-2.component.html',
+  styleUrl: './test-lib-2.component.css',
+})
+export class TestLib2Component {}
